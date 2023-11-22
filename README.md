600c158cabeb6884e64cbd7fd9d73f81aef246f8
Jonas Schöley <jschoeley@gmail.com>
2023-11-22 09:39:22 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Predictive Modeling
+
+Jonas Schöley
+
+Materials related to the PHDS23 seminar on "Predictive Modeling".
+
+![](ass/teaser.png)
---
+++ src/00-install_dependencies.R
@@ -0,0 +1,7 @@
+libs <- c('tidyverse', 'MASS', 'mgcv', 'showtext')
+
+install.packages(
+  libs,
+  repos = c(getOption('repos')),
+  dep = TRUE
+)
